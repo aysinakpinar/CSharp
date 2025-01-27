16b5ab6c45a57299457db4cda48d1e70ffc91673
Aysin <aysinn.akpinar@gmail.com>
2025-01-27 17:10:34 +0000

    Instance Methods Exercises

---
+++ development/CSharp.InstanceMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharp.InstanceMethods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(myStaticMethod());
+        // create an instance of Program and assign it to a variable
+        Program myProgram = new Program();
+        myProgram.myMethod("new instance text");
+        
+    }
+
+    static string myStaticMethod(){
+        return "a static method";
+    }
+
+    void myMethod(){
+        Console.WriteLine("an instance method");
+    }
+
+    void myMethod(string message){
+        Console.WriteLine(message);
+    }
+}

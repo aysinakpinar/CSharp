82781f0038995e901605e47eeee4243e1f480ad9
Aysin <aysinn.akpinar@gmail.com>
2025-01-28 17:00:58 +0000

    If Statements, FizzBuzz

---
+++ development/CSharp.IfStatements/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CSharp.IfStatements;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FizzBuzz.Play(1); // => "1"
+        FizzBuzz.Play(3); // => "Fizz"
+        FizzBuzz.Play(5); // => "Buzz"
+        FizzBuzz.Play(15); //=> "FizzBuzz"
+    }
+}
+
+static class FizzBuzz
+{
+    public static void Play(int number)
+    {
+        if(number % 3 == 0 && number % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if(number %3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (number % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else 
+        {
+            Console.WriteLine($"{number}");
+        }
+
+    }
+
+}

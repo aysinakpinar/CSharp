e192ee3a146c14dcf14c7621355f40cd19bc8002
Aysin <aysinn.akpinar@gmail.com>
2025-01-28 16:35:42 +0000

    Strings exercises and challenges

---
+++ development/CSharp.StringStoryBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharp.ArithmeticStoryBuilder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        StoryBuilder newStory = new StoryBuilder("Hello this is the initial Plot");
+        newStory.AddPlotline("Adding to the original plotline");
+        newStory.GetPlot();
+    }
+}
+
+class StoryBuilder
+{
+    string plot;
+    internal StoryBuilder(string plot)
+    {
+        this.plot = plot;
+    }
+
+    internal void AddPlotline(string plotLine)
+    {
+        plot = $"{plot} {plotLine}";
+    }
+
+    internal void GetPlot()
+    {
+        Console.WriteLine(plot);
+    }
+}

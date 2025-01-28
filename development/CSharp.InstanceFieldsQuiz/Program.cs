a230b0fd27b56ec7a5ceb9126dcb0b6ae66f0e5f
Aysin <aysinn.akpinar@gmail.com>
2025-01-28 14:13:28 +0000

    Instance Fields

---
+++ development/CSharp.InstanceFieldsQuiz/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharp.InstanceFieldsQuiz;
+
+class Quiz
+{
+    static void Main(string[] args)
+    {
+        Quiz quiz = new Quiz("what is the capital of Burkina Faso?", "What is the capital of Bhutan?");
+        quiz.getQuestionOne();
+        quiz.getQuestionTwo();
+    }
+    string questionOne;
+    string questionTwo;
+    Quiz(string questionOne, string questionTwo)
+    {
+        this.questionOne = questionOne;
+        this.questionTwo = questionTwo;
+    }
+
+    private void getQuestionOne()
+    {
+        Console.WriteLine($"{questionOne}");
+    }
+
+    private void getQuestionTwo()
+    {
+        Console.WriteLine($"{questionTwo}");
+    }
+}

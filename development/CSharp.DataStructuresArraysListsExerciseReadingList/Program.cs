3afc5da132b32021e069b8a867ecb5b66d0e4cf9
Aysin <aysinn.akpinar@gmail.com>
2025-01-30 16:49:04 +0000

    Dictionaries

---
+++ development/CSharp.DataStructuresArraysListsExerciseReadingList/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace CSharp.DataStructuresArraysListsExerciseReadingList;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ReadingList unreadList = new ReadingList();
+        unreadList.Add("Lord of the Rings");  
+        unreadList.Add("1984");
+        unreadList.Add("Brave New World"); 
+        unreadList.DisplayBooks();
+    }
+}
+internal class ReadingList
+{
+    string [] unread = new string [2];
+    int count = 0;
+    internal void Add(string newBook)
+    { 
+        if(count >= unread.Length)
+        {
+            ExpandArray();
+        }
+        unread[count] = newBook;
+        count++;
+    }
+    internal void ExpandArray()
+    {
+        int newSize = unread.Length * 2;
+        string [] newUnread = new string[newSize];
+        for (int i =0; i < unread.Length; i++)
+        {
+            newUnread[i] = unread[i];
+        }
+        unread = newUnread;
+    }
+    internal void DisplayBooks()
+    {
+        foreach(string book in unread)
+        {
+            if (!string.IsNullOrEmpty(book))
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+
+}

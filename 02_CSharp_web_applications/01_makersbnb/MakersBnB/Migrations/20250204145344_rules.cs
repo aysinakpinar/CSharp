c6592eca23ec661ec35738ee2cd96ebbc2b77dc5
Aysin <aysinn.akpinar@gmail.com>
2025-02-05 12:00:00 +0000

    Update 20250204145344_Rules.cs
    class name is updated

--- 02_CSharp_web_applications/01_makersbnb/MakersBnB/Migrations/20250204145344_rules.cs
+++ 02_CSharp_web_applications/01_makersbnb/MakersBnB/Migrations/20250204145344_rules.cs
@@ -5,7 +5,7 @@
 namespace MakersBnB.Migrations
 {
     /// <inheritdoc />
-    public partial class rules : Migration
+    public partial class Rules : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
